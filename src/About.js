aec4e23b7647365f4a8c53a86d81a40649fd1d34
Harsh Pandey <135162038+Harshpandey22@users.noreply.github.com>
2023-10-15 00:31:55 +0530

    Add files via upload

---
+++ src/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="section">
+      <h2>About Me</h2>
+      <p>3rd year Computer Science studentpurshuing Btech at Vellore Instituteof Technology with a lot of passion inData Science and Full StackDevelopment field.</p>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Contact.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Contact = () => {
+  return (
+    <div className="section">
+      <h2>Contact Me</h2>
+      <p>Email: harsh.pandey2021a@vitstudent.ac.in</p>
+      <p>Phone: 8874328862</p>
+      <p>LinkedIn:https://www.linkedin.com/in/harsh-pandey-fd21/</p>
+    </div>
+  );
+};
+
+export default Contact;
